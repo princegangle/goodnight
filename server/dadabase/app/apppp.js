1d954b61709364f095c45d4ac63f7131c9ba947b
dont knew <princegangle@gmail.com>
2022-12-20 19:07:46 +0530

    first commit

---
+++ server/dadabase/app/apppp.js
@@ -0,0 +1,6 @@
+console.log("hello")
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/princegangle/goodnight.git
+git push -u origin main
